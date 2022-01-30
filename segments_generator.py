0e50a240c3322636c54cf724c134271c72b18d8d
alonCitrusX <alon@citrusx.ai>
2022-01-30 16:30:36 +0200

    working segment generator

--- segments_generator.py
+++ segments_generator.py
@@ -2,34 +2,20 @@ import numpy as np
 from typing import List
 from segment import Segment
 import itertools
-from type import Type
 
 
 class SegmentsGenerator:
     def __init__(self, d: int, b_low: List, b_high: List):
-        x_low = [np.arange(b_low[i], b_high[i] - 1, 1) for i in range(d)]
-        x_high = [np.arange(b_low[i] + 1, b_high[i], 1) for i in range(d)]
-        low_edges = []
-        high_edges = []
-        for low_edge in itertools.product(*x_low):
-            low_edges.append(low_edge)
-        for high_edge in itertools.product(*x_high):
-            high_edges.append(high_edge)
-        segments_list = []
-        for i in range(len(low_edges)):
-            segments_list.append(Segment(d, low_edges[i], high_edges[i]))
-        self._segments = segments_list
+        self._segments = self.generate_segments(d, b_low, b_high)
 
     @property
     def segments(self):
         return self._segments
 
-#
-# segments = SegmentsGenerator(2, np.array([2, 5]), np.array([8, 9]))
-# print(type(segments))
-# p = segments.segments
-#
-# print(type(p))
-#
-# for segment in p:
-#     print(segment.get_data())
+    @staticmethod
+    def generate_segments(d: int, b_low: List, b_high: List) -> List[Segment]:
+        x_low = [np.arange(b_low[i], b_high[i] - 1, 1) for i in range(d)]
+        x_high = [np.arange(b_low[i] + 1, b_high[i], 1) for i in range(d)]
+        low_edges = [low_edge for low_edge in itertools.product(*x_low)]
+        high_edges = [high_edge for high_edge in itertools.product(*x_high)]
+        return [Segment(d, low_edges[i], high_edges[i]) for i in range(len(low_edges))]
