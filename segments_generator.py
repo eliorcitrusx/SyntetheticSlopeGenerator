2a88adaa6c73f9d8e35547b2a5e6a9c163bd9b32
alonCitrusX <alon@citrusx.ai>
2022-01-30 15:45:28 +0200

    changed segment generator

--- segments_generator.py
+++ segments_generator.py
@@ -1,6 +1,7 @@
 import numpy as np
 from typing import List
 from segment import Segment
+import itertools
 from type import Type
 
 
@@ -9,21 +10,37 @@ class SegmentsGenerator:
         x_low = [[] for _ in range(d)]
         x_high = [[] for _ in range(d)]
         for i in range(d):
-            x_low[i] = range(b_low[i], b_high[i] - 1, 1)
-            x_high[i] = range(b_low[i] + 1, b_high[i], 1)
+            x_low[i] = np.arange(b_low[i], b_high[i] - 1, 1)
+            x_high[i] = np.arange(b_low[i] + 1, b_high[i], 1)
             print(i)
         grid_low_edge = np.meshgrid(*x_low)
         grid_high_edge = np.meshgrid(*x_high)
         segment_vec = np.vectorize(Segment)
-        self._segments = segment_vec(d, x_low, x_high)
-
-    def get_segments(self):
+        low_edges = []
+        high_edges = []
+        counter = 0
+        for low_edge in itertools.product(*x_low):
+            low_edges.append(low_edge)
+            counter += 1
+        counter = 0
+        for high_edge in itertools.product(*x_high):
+            counter += 1
+            high_edges.append(high_edge)
+        for i in range (len(low_edges)):
+            print(low_edges[i], high_edges[i])
+        segments=[]
+        for index in range(len(low_edges)):
+            segments.append(Segment(d, low_edge, high_edge))
+        self._segments = segments
+    @property
+    def segments(self):
         return self._segments
 
 
-segments = SegmentsGenerator(2, np.array([2, 2]), np.array([4, 4]))
-p = segments.get_segments()[0]
+segments = SegmentsGenerator(2, np.array([2, 5]), np.array([8, 9]))
+p = segments.get_segments()
 
+print(p[0])
 print(p[0].get_data())
 
 #print(p[0])
